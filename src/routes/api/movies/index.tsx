f785daf000863962055f9168a59e859e5e975c90
Shashkov Danil <shashkovdanil@gmail.com>
2023-08-31 00:59:55 +0100

    debug

--- src/routes/api/movies/index.tsx
+++ src/routes/api/movies/index.tsx
@@ -4,15 +4,21 @@ import type { MoviesResponse } from '~/domain/movie'
 import { isFutureDate } from '~/lib/isFutureDate'
 
 export const onGet: RequestHandler = async ({ json, url }) => {
-  const from = url.searchParams.get('from') as string
-  const to = url.searchParams.get('to') as string
+  try {
+    const from = url.searchParams.get('from') as string
+    const to = url.searchParams.get('to') as string
 
-  const URL = isFutureDate(from)
-    ? `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&primary_release_date.gte=${from}&primary_release_date.lte=${to}`
-    : `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&primary_release_date.gte=${from}&primary_release_date.lte=${to}&vote_average.gte=5&vote_count.gte=100`
+    console.log(process.env.TMDB_API_KEY)
+    const URL = isFutureDate(from)
+      ? `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&primary_release_date.gte=${from}&primary_release_date.lte=${to}`
+      : `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&primary_release_date.gte=${from}&primary_release_date.lte=${to}&vote_average.gte=5&vote_count.gte=100`
 
-  const response = await fetch(URL)
-  const data: MoviesResponse = await response.json()
+    console.log(URL)
+    const response = await fetch(URL)
+    const data: MoviesResponse = await response.json()
 
-  json(200, releasesAdapter({ type: 'movies', response: data }))
+    json(200, releasesAdapter({ type: 'movies', response: data }))
+  } catch (error) {
+    console.error(error)
+  }
 }
