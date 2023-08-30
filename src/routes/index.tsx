dfa11a6065657a4aa338cee2aa3072d67c72065d
Shashkov Danil <shashkovdanil@gmail.com>
2023-08-31 00:37:19 +0100

    fixed ts error

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,59 +1,29 @@
 import { component$ } from '@builder.io/qwik'
 import { routeLoader$ } from '@builder.io/qwik-city'
 import type { DocumentHead } from '@builder.io/qwik-city'
-import { LinkTabs } from '~/components/LinkTabs'
-import { Calendar } from '~/components/Calendar'
-import { Title } from '~/components/Title'
 import { getReleases } from '~/services/api'
 import { getAdjacentMonthDetails } from '~/lib/getAdjacentMonthDetails'
+import { ReleasesLayout } from '~/components/ReleasesLayout'
 
-const LINKS = [
-  {
-    name: 'Movies',
-    href: '/movies',
-  },
-  {
-    name: 'TV',
-    href: '/tv',
-  },
-  {
-    name: 'Games',
-    href: '/games',
-  },
-]
-
-export const useData = routeLoader$(async () => {
+export const useData = routeLoader$(async params => {
   const {
     current: { monthRange, year, monthIndex },
   } = getAdjacentMonthDetails()
 
-  const movies = await getReleases('movies', monthRange)
+  const releases = await getReleases('movies', monthRange)
 
   return {
-    movies,
+    releases,
     year,
     month: monthIndex,
+    date: params.params.date,
   }
 })
 
 export default component$(() => {
   const data = useData()
 
-  return (
-    <>
-      <div class="mb-5">
-        <LinkTabs data={LINKS} />
-      </div>
-      <div class="mb-8">
-        <Title level={1}>August 2023 Movie Releases</Title>
-      </div>
-      <Calendar
-        month={data.value.month}
-        year={data.value.year}
-        releases={data.value.movies}
-      />
-    </>
-  )
+  return <ReleasesLayout type="movies" {...data.value} />
 })
 
 export const head: DocumentHead = {
