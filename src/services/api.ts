8fcff35d9f0227d06b5017c918c7cd96932febb6
Shashkov Danil <shashkovdanil@gmail.com>
2023-08-31 00:40:20 +0100

    used host from env

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import type { ReleaseType, Release } from '~/domain/release'
 
 export async function getReleases(type: ReleaseType, range: [string, string]) {
   const res = await fetch(
-    `http://localhost:5173/api/${type}/?from=${range[0]}&to=${range[1]}`
+    `${process.env.HOST}/api/${type}/?from=${range[0]}&to=${range[1]}`
   )
   const json = await res.json()
 
