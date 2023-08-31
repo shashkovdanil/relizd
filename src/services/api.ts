22322e483b40c13579619e8bc80d7547715f40ce
Shashkov Danil <shashkovdanil@gmail.com>
2023-08-31 01:10:28 +0100

    debug

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import type { ReleaseType, Release } from '~/domain/release'
 
 export async function getReleases(type: ReleaseType, range: [string, string]) {
-  console.log('process.env', process.env, 'process.env.HOST', process.env.HOST)
+  console.log('process.env', process.env, 'process.env.HOST', process.env['HOST'])
   console.log('url', `${process.env.HOST}/api/${type}/?from=${range[0]}&to=${range[1]}`)
   const res = await fetch(
     `${process.env.HOST}/api/${type}/?from=${range[0]}&to=${range[1]}`
